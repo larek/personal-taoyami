6cfaa13e4fe743b474b753753aa116cdf0bfa09e
Aleksandr Tikhonov <t9101029991@gmail.com>
2020-12-22 19:39:00 +0300

    update education page

--- pages/education.js
+++ pages/education.js
@@ -6,33 +6,49 @@ export default function education() {
   return (
     <Layout title={title}>
       <Content title={title}>
-        <table className='table table-bordered'>
-          <tbody>
-            <tr className='table-secondary'>
-              <td>Высшее образование</td>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>Высшее образование</th>
             </tr>
+          </thead>
+          <tbody>
             <tr>
               <td>
                 УРАО (Университет Российской Академии Образования).
                 Факультет «Связи с общественностью»
             </td>
             </tr>
-            <tr className='table-secondary'>
-              <td>Курсы повышения квалификации</td>
+          </tbody>
+        </table>
+
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>Курсы повышения квалификации</th>
             </tr>
+          </thead>
+          <tbody>
             <tr><td>Курс «SMM-специалист» от <a href='https://netology.ru/programs/smm-specialist' target='_blank'>«Нетология»</a></td></tr>
             <tr><td>Курс «UX c нуля как стать проектировщиком интерфейсов»</td></tr>
             <tr><td>Курс «Реальный Директ 2.0»</td></tr>
             <tr><td>Курс «Реальный Маркетинг»</td></tr>
             <tr><td>Детальное изучение бесплатных и платных материалов главреда Максима Ильяхова, материалов <a href="https://bureau.ru" target='_blank'>«Бюро Горбунова»</a> и материалов, рекомендованных <a href="https://bureau.ru/school/editors/13/" target='_blank'>«Школой редакторов»</a>.</td></tr>
-            <tr className='table-secondary'>
-              <td>Текущее обучение</td>
-            </tr>
-            <tr><td>Курс по созданию анимации в Adobe After Effects.</td></tr>
           </tbody>
         </table>
-        <p>
-        </p>
+
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>Текущее обучение</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Курс по созданию анимации в Adobe After Effects.</td>
+            </tr>
+          </tbody>
+        </table>
 
       </Content>
     </Layout>
