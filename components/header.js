b9a2280bfff2ade83a49aa5df68b0dbb4d99db12
Alexandr Tikhonov <t9101029991@gmail.com>
2021-01-08 00:55:06 +0300

    correct toggle btn place

--- components/header.js
+++ components/header.js
@@ -12,51 +12,53 @@ export default function header() {
   const router = useRouter();
 
   return (
-    <header id="header">
-      <div className="d-flex flex-column">
-        <div className="profile">
-          <img src='/img/profile-pic.jpg' className='img-fluid rounded-circle image-loader' />
-          <h1 className="text-light text-center"><a href="index.html">Татьяна Савина</a></h1>
-          <div className='mt-1 text-center'><a className='text-white' href='mailto:taoyami@yandex.ru'>taoyami@yandex.ru</a></div>
-          <div className='mt-1 text-center'><a className='text-white' href='tel:+79601641052'>+79601641052</a></div>
-          <div className="social-links mt-1 text-center">
-            <a href="https://facebook.com/taoyami1" target="_blank"><img src='/img/facebook.svg' className='img-fluid' /></a>
-            <a href="https://instagram.com/taoyami" target="_blank"><img src='/img/instagram.svg' className='img-fluid' /></a>
-            <a href="https://vk.com/taoyami" target="_blank"><img src='/img/vk.svg' className='img-fluid' /></a>
-          </div>
-        </div>
+    <div>
 
-        <nav className="nav-menu">
-          <ul>
-            <li className={router.pathname === '/' ? 'active' : ''}>
-              <Link href='/'><a href='/'>
-                <i className='bi bi-house'></i>
+      <header id="header">
+        <div className="d-flex flex-column">
+          <div className="profile">
+            <img src='/img/profile-pic.jpg' className='img-fluid rounded-circle image-loader' />
+            <h1 className="text-light text-center"><a href="index.html">Татьяна Савина</a></h1>
+            <div className='mt-1 text-center'><a className='text-white' href='mailto:taoyami@yandex.ru'>taoyami@yandex.ru</a></div>
+            <div className='mt-1 text-center'><a className='text-white' href='tel:+79601641052'>+79601641052</a></div>
+            <div className="social-links mt-1 text-center">
+              <a href="https://facebook.com/taoyami1" target="_blank"><img src='/img/facebook.svg' className='img-fluid' /></a>
+              <a href="https://instagram.com/taoyami" target="_blank"><img src='/img/instagram.svg' className='img-fluid' /></a>
+              <a href="https://vk.com/taoyami" target="_blank"><img src='/img/vk.svg' className='img-fluid' /></a>
+            </div>
+          </div>
+
+          <nav className="nav-menu">
+            <ul>
+              <li className={router.pathname === '/' ? 'active' : ''}>
+                <Link href='/'><a href='/'>
+                  <i className='bi bi-house'></i>
                 Главная
                 </a></Link>
-            </li>
-            <li className={router.pathname === '/education' ? 'active' : ''}>
-              <Link href='education'><a href='/education'>
-                <i className='bi bi-bell'></i>
+              </li>
+              <li className={router.pathname === '/education' ? 'active' : ''}>
+                <Link href='education'><a href='/education'>
+                  <i className='bi bi-bell'></i>
                 Образование
                 </a></Link>
-            </li>
-            <li className={router.pathname === '/skills' ? 'active' : ''}>
-              <Link href='skills'><a href='/skills'>
-                <i className='bi bi-star'></i>
+              </li>
+              <li className={router.pathname === '/skills' ? 'active' : ''}>
+                <Link href='skills'><a href='/skills'>
+                  <i className='bi bi-star'></i>
                 Навыки
                 </a></Link>
-            </li>
-            <li className={router.pathname === '/resume' ? 'active' : ''}>
-              <Link href='resume'><a href='/resume'>
-                <i className='bi bi-file-earmark-text'></i>
+              </li>
+              <li className={router.pathname === '/resume' ? 'active' : ''}>
+                <Link href='resume'><a href='/resume'>
+                  <i className='bi bi-file-earmark-text'></i>
                 Опыт работы
                 </a></Link>
-            </li>
-          </ul>
-        </nav>
-        <button type="button" onClick={toggle} className="mobile-nav-toggle d-xl-none"><i className="bi bi-list"></i></button>
-
-      </div>
-    </header>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <button type="button" onClick={toggle} className="mobile-nav-toggle d-xl-none"><i className="bi bi-list"></i></button>
+    </div>
   );
 }
